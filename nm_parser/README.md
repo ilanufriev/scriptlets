c0d8b08944853aeb5d32451821b3d5d3244102df
user <user@user.com>
2025-03-28 16:29:25 +0300

    Added nm_parser.py

---
+++ nm_parser/README.md
@@ -0,0 +1,81 @@
+# nm_parser
+
+This is a little scriptlet that parses the output of this command:
+
+```console
+$ nm --size-sort --print-size FILENAME
+0000000000009058 0000000000000001 b completed.0
+00000000000024bf 000000000000000b T SomeFunction
+0000000000001962 000000000000002e t SomeStaticFunction
+0000000000001a48 0000000000000034 T AnotherFunction
+0000000000003340 0000000000000040 R AConstant1
+0000000000003580 0000000000000040 R AConstant2
+00000000000035c0 0000000000000040 R AConstant3
+0000000000003600 0000000000000040 R AConstant4
+0000000000003380 0000000000000040 R AConstant5
+00000000000033c0 0000000000000040 R AConstant6
+...
+```
+
+The output of the parse process is a table with total sizes of symbols of each type. This script may be helpful to determine the amount of bytes that static data takes up in the executable or library file.
+
+## Usage
+
+To see usage, use -h flag:
+
+```console
+$ python3 nm_parser.py -h
+usage: nm_parser.py [-h] [--csv] [--run-nm FILENAME]
+
+nm output parser for nm --size-sort --print-size
+
+options:
+  -h, --help         show this help message and exit
+  --csv              output data in the .csv format
+  --run-nm FILENAME  run nm --size-sort --print-size <FILE> and parse output
+```
+
+## Examples
+
+There are two ways of using nm_parser.py
+
+### 1. Pipe output of the nm command into it
+
+Like this:
+
+```console
+$ nm --size-sort --print-size /path/to/file | python3 nm_parser.py
+Reading from stdin
+
+Result:
+Type    Total size, Bytes    Total size (hex), bytes    Type Description
+b       65537                0x10001                    Symbol in the uninitialized data section (BSS) with local (static) scope.
+B       35                   0x23                       Symbol in the uninitialized data section (BSS) with global scope.
+D       20                   0x14                       Symbol in the initialized data section with global scope.
+R       4                    0x4                        Symbol in the read-only data section with global scope.
+r       37                   0x25                       Symbol in the read-only data section with local (static) scope.
+T       1202                 0x4b2                      Symbol in the text (code) section with global scope (often functions).
+t       145                  0x91                       Symbol in the text (code) section with local (static) scope.
+d       240                  0xf0                       Symbol in the initialized data section with local (static) scope.
+
+```
+
+### 2. Run nm from the script and parse its input
+
+Like this:
+
+```console
+python3 nm_parser.py --run-nm /path/to/file
+Running command: nm --size-sort --print-size /path/to/file
+
+Result:
+Type    Total size, Bytes    Total size (hex), bytes    Type Description
+b       65537                0x10001                    Symbol in the uninitialized data section (BSS) with local (static) scope.
+B       35                   0x23                       Symbol in the uninitialized data section (BSS) with global scope.
+D       20                   0x14                       Symbol in the initialized data section with global scope.
+R       4                    0x4                        Symbol in the read-only data section with global scope.
+r       37                   0x25                       Symbol in the read-only data section with local (static) scope.
+T       1202                 0x4b2                      Symbol in the text (code) section with global scope (often functions).
+t       145                  0x91                       Symbol in the text (code) section with local (static) scope.
+d       240                  0xf0                       Symbol in the initialized data section with local (static) scope.
+```
---
+++ nm_parser/src/nm_parser.py
@@ -0,0 +1,153 @@
+#!/bin/python3
+
+import subprocess
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(
+            description="nm output parser for nm --size-sort --print-size"
+        )
+parser.add_argument('--csv', action='store_true', help='output data in the .csv format')
+parser.add_argument('--run-nm', type=str, help='run nm --size-sort --print-size <FILE> and parse output', dest="FILENAME")
+args = parser.parse_args()
+
+g_nm_tags = {
+    "A": "Absolute symbol. Not affected by relocation. Typically used for constants.",
+    "B": "Symbol in the uninitialized data section (BSS) with global scope.",
+    "b": "Symbol in the uninitialized data section (BSS) with local (static) scope.",
+    "C": "Common symbol. Uninitialized variable (often with common linkage).",
+    "D": "Symbol in the initialized data section with global scope.",
+    "d": "Symbol in the initialized data section with local (static) scope.",
+    "G": "Symbol in the initialized data section for small objects with global scope.",
+    "g": "Symbol in the initialized data section for small objects with local (static) scope.",
+    "I": "Indirect symbol, pointing to another symbol.",
+    "N": "Debugging symbol.",
+    "R": "Symbol in the read-only data section with global scope.",
+    "r": "Symbol in the read-only data section with local (static) scope.",
+    "S": "Symbol in the uninitialized data section for small objects with global scope.",
+    "s": "Symbol in the uninitialized data section for small objects with local (static) scope.",
+    "T": "Symbol in the text (code) section with global scope (often functions).",
+    "t": "Symbol in the text (code) section with local (static) scope.",
+    "U": "Undefined symbol, referenced but not defined in the object file.",
+    "W": "Weak symbol with global scope.",
+    "w": "Weak symbol with local (static) scope.",
+    "V": "Weak object symbol in the COMDAT group with global scope.",
+    "v": "Weak object symbol in the COMDAT group with local (static) scope.",
+    "?": "Unknown or unidentified symbol type."
+}
+
+class NMSymbolData:
+    offset: int
+    size:   int
+    name:   str
+    type:   str
+
+    def __init__(self, sym_offset: int, sym_size: int,
+                       sym_type: str,   sym_name: str):
+        self.offset = sym_offset
+        self.size   = sym_size
+        self.name   = sym_name
+        self.type   = sym_type
+
+    def __str__(self):
+        return f'NMSymbolData[ Name: {self.name} Type: {self.type} ' + \
+               f'Offset: {self.offset} (hex: {hex(self.offset)}) '   + \
+               f'Size: {self.size} (hex: {hex(self.size)}) ]'
+
+def nm_parse(input: str) -> list[NMSymbolData]:
+    input_lines = input.strip().split('\n')
+    tokens: list[list[str]] = []
+    data = []
+
+    for line in input_lines:
+        tokens.append(line.strip().split(' '))
+
+    for line_tokens in tokens:
+        data.append(NMSymbolData(sym_offset = int(line_tokens[0], 16),
+                                 sym_size   = int(line_tokens[1], 16),
+                                 sym_type   = line_tokens[2],
+                                 sym_name   = line_tokens[3]))
+    return data
+
+def nm_get_total_size_per_type(data: list[NMSymbolData]) -> dict[str, str]:
+    totals: dict[str, int] = {}
+
+    for d in data:
+        if not d.type in totals.keys():
+            totals[d.type] = d.size
+            continue
+
+        totals[d.type] += d.size
+
+    return totals
+
+def format_table(table:list[list], padding = 4):
+    out = ''
+    is_first_line = True
+    for line in table:
+        is_first = True
+        if not is_first_line:
+            out += '\n'
+
+        for i in range(0, len(line)):
+            if not is_first:
+                out += ' ' * padding
+
+            is_first = False
+            out += line[i].ljust(len(table[0][i]))
+
+        is_first_line = False
+
+    return out
+
+def cmd_to_str(cmd: list[str]):
+    return ' '.join(map(str, cmd))
+
+def main(argv):
+    input: str
+
+    if args.FILENAME:
+        cmd = ['nm', '--size-sort', '--print-size', args.FILENAME]
+
+        print(f'Running command: {cmd_to_str(cmd)}')
+        result = subprocess.run(cmd, capture_output=True, text=True)
+        if result.returncode != 0:
+            print(f'Failed to run command {cmd}. Error:\n{result.stderr}')
+
+        input = result.stdout
+    else:
+        print(f'Reading from stdin')
+        input = sys.stdin.read()
+        if len(input) == 0:
+            return 0
+
+    data = nm_parse(input)
+    totals = nm_get_total_size_per_type(data)
+    sep = '\t'
+    table: list[list[str]] = [['Type', 'Total size, Bytes', 'Total size (hex), bytes', 'Type Description']]
+    padding = 4
+
+    for type in totals.keys():
+        table.append([type, str(totals[type]), str(hex(totals[type])), g_nm_tags[type]])
+
+    if args.csv:
+        sep = ';'
+        for line in table:
+            is_first = True
+            for item in line:
+                if not is_first:
+                    print(sep, end='')
+
+                is_first = False
+                print(item, end='')
+
+            print()
+        return 0
+
+    print('\nResult:')
+    print(format_table(table))
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
